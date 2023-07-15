8b4fd4d67cd44072d2909ef4ed073d339dc85f72
Supakorn <supakorn0728@gmail.com>
2023-07-15 20:54:37 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import Footer from "@/components/Footer";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import Navbar from "@/components/Navbar";
+
+const inter = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Shop",
+  description: "Shop"
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
