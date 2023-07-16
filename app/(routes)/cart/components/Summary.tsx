5044b4acbb1c79ceaf316e89746f9157b79f10a3
Supakorn <supakorn0728@gmail.com>
2023-07-17 00:51:01 +0700

    updated

--- app/(routes)/cart/components/Summary.tsx
+++ app/(routes)/cart/components/Summary.tsx
@@ -29,6 +29,7 @@ const Summary = () => {
     }, 0);
 
     const onCheckout = async () => {
+        if (!items.length) return toast.error("Cart is empty");
         const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/checkout`, {
             productIds: items.map((item) => item.id)
         });
